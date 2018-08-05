ae70d80f3709d66bcb198bb9d95c0263efe16113
DaJuukes <dajuukes@gmail.com>
2018-08-04 18:06:57 -0700

    fix init msg

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -6,8 +6,8 @@ exports.run = (bot, guild) => {
     My default prefix here is \`%\`.\n
 You can see a list of commands by typing \`%help\`.\n
 You can change my prefix with \`%setprefix <newprefix>\`.\n
-If you need help, feel free to join our support server at https://discord.gg/Xg5V8mn \n
-If you like my features, I'd really appreciate a free and quick upvote on our Bot List over [here](https://discordbots.org/bot/411996950670344234)!`
+If you need help, feel free to join our support server at ${process.env.SUPPORT_SERVER} \n
+If you like my features, I'd really appreciate a free and quick upvote on our Bot List over [here](${process.env.UPVOTE_LINK})!`
 
   emb.setTitle('Thank you for adding me!')
     .setColor(`#00FF00`)
